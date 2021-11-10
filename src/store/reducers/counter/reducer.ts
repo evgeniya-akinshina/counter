cef0d40a8d36d212413a113aa0c81227c52bc9e4
Aiscaat <maks.kolendyuk@gmail.com>
2021-11-10 14:46:11 +0200

    3

--- src/store/reducers/counter/reducer.ts
+++ src/store/reducers/counter/reducer.ts
@@ -1,26 +1,25 @@
-/* eslint-disable no-duplicate-case */
-import { CounterState as State, CounterTypes } from './types';
+import { CounterState as State, CounterTypes } from './types'
 
 const initialState: State = {
 	value1: 0,
-    value2: 0,
+	value2: 0,
 }
 
 export const counterReducer = (state: State = initialState, action: any) => {
 	switch (action.type) {
 		case CounterTypes.INCREMENT:
-			return { ...state, value: state.value1 + 1 }
+			return { ...state, value1: state.value1 + 1 }
 
 		case CounterTypes.DECREMENT:
-			return { ...state, value: state.value1 - 1 }
+			return { ...state, value1: state.value1 - 1 }
 
-        case CounterTypes.INCREMENT1:
-            return { ...state, value: state.value2 + 1 }
+		case CounterTypes.INCREMENT1:
+			return { ...state, value2: state.value2 + 1 }
 
-        case CounterTypes.DECREMENT1:
-            return { ...state, value: state.value2 - 1 }
+		case CounterTypes.DECREMENT1:
+			return { ...state, value2: state.value2 - 1 }
 
 		default:
 			return state
 	}
-};
+}
--- src/store/reducers/counter/types.ts
+++ src/store/reducers/counter/types.ts
@@ -1,11 +1,11 @@
 export type CounterState = {
-    value1: number,
-    value2: number,
+	value1: number
+	value2: number
 }
 
 export enum CounterTypes {
 	INCREMENT = 'counter|increment',
 	DECREMENT = 'counter|decrement',
-    INCREMENT1 = 'counter|increment1',
+	INCREMENT1 = 'counter|increment1',
 	DECREMENT1 = 'counter|decrement1',
-};
+}
